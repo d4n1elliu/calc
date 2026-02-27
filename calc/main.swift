9470227289ffcd8491e1a3341bf1aa3ed28cafed
Daniel Liu <143345383+d4n1elliu@users.noreply.github.com>
2026-02-27 13:44:34 +1100

    Updated main.swift file

--- calc/main.swift
+++ calc/main.swift
@@ -11,6 +11,15 @@ import Foundation
 var args = ProcessInfo.processInfo.arguments
 args.removeFirst() // remove the name of the program
 
+// Retrieve User Input
+//let no1 = args[0]; // Sample Code Only! Update Required!
+//let operator = args[1]; // Sample Code Only! Update Required!
+//let no2 = args[2]; // Sample Code Only! Update Required!
 
+// Initialize a Calculator object
+let calculator = Calculator();
 
-print(Int(args[0])!)
+// Calculate the result
+let result = calculator.add(no1: 1, no2: 1);
+
+print(result)
